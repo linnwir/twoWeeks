2637fc8cb2a5ab0a9971de35570fba8baf806b6b
linnwir <linn.wir@gmail.com>
2016-05-03 15:08:27 +0200

    bild på 2 första veckor

---
+++ 0429, VB/Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGissa = new System.Windows.Forms.Button();
+            this.btnNyttOrd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblSlutResultat = new System.Windows.Forms.Label();
+            this.btnGissaHelaOrdet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.taOrd = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGissa
+            // 
+            this.btnGissa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGissa.Location = new System.Drawing.Point(851, 217);
+            this.btnGissa.Name = "btnGissa";
+            this.btnGissa.Size = new System.Drawing.Size(89, 38);
+            this.btnGissa.TabIndex = 1;
+            this.btnGissa.Text = "Gissa";
+            this.btnGissa.UseVisualStyleBackColor = true;
+            this.btnGissa.Click += new System.EventHandler(this.btnGissa_Click);
+            // 
+            // btnNyttOrd
+            // 
+            this.btnNyttOrd.Location = new System.Drawing.Point(0, 0);
+            this.btnNyttOrd.Name = "btnNyttOrd";
+            this.btnNyttOrd.Size = new System.Drawing.Size(75, 23);
+            this.btnNyttOrd.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(691, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bokstäver i ordet: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(789, 231);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(448, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Gissa hela ordet";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(445, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // lblSlutResultat
+            // 
+            this.lblSlutResultat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSlutResultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSlutResultat.Location = new System.Drawing.Point(451, 29);
+            this.lblSlutResultat.Name = "lblSlutResultat";
+            this.lblSlutResultat.Size = new System.Drawing.Size(433, 29);
+            this.lblSlutResultat.TabIndex = 8;
+            // 
+            // btnGissaHelaOrdet
+            // 
+            this.btnGissaHelaOrdet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGissaHelaOrdet.Location = new System.Drawing.Point(424, 231);
+            this.btnGissaHelaOrdet.Name = "btnGissaHelaOrdet";
+            this.btnGissaHelaOrdet.Size = new System.Drawing.Size(172, 37);
+            this.btnGissaHelaOrdet.TabIndex = 9;
+            this.btnGissaHelaOrdet.Text = "Gissa hela ordet";
+            this.btnGissaHelaOrdet.UseVisualStyleBackColor = true;
+            this.btnGissaHelaOrdet.Click += new System.EventHandler(this.btnGissaHelaOrdet_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(691, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Skriv bokstav:";
+            // 
+            // taOrd
+            // 
+            this.taOrd.Location = new System.Drawing.Point(520, 330);
+            this.taOrd.Name = "taOrd";
+            this.taOrd.Size = new System.Drawing.Size(172, 23);
+            this.taOrd.TabIndex = 12;
+            this.taOrd.Text = "Hämta Ord";
+            this.taOrd.UseVisualStyleBackColor = true;
+            this.taOrd.Click += new System.EventHandler(this.ta_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hangman.Properties.Resources.Galge;
+            this.pictureBox1.InitialImage = global::Hangman.Properties.Resources.Galge;
+            this.pictureBox1.Location = new System.Drawing.Point(80, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 320);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 494);
+            this.Controls.Add(this.taOrd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGissaHelaOrdet);
+            this.Controls.Add(this.lblSlutResultat);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnNyttOrd);
+            this.Controls.Add(this.btnGissa);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGissa;
+        private System.Windows.Forms.Button btnNyttOrd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblSlutResultat;
+        private System.Windows.Forms.Button btnGissaHelaOrdet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button taOrd;
+    }
+}
+
---
+++ 0429, VB/Hangman/Hangman/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        int guessNumber = 1;
+        char guess;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string rightAnswer = null;
+        string rightAnswer2 = null;
+    }
+
+        string returnWordSwedish()
+        {
+            Random rnd1 = new Random();
+            string[] allLines = File.ReadAllLines("C:\\Bibliotek\\Swedish.txt");
+            string rightWord = allLines[rnd1.Next(allLines.Length)];
+            return rightWord;
+        }
+
+
+        private void btnGissa_Click(object sender, EventArgs e) //Knapp för att gissa
+        {
+            guess = char.Parse(textBox2.Text); //Här gör man sin gissning med en bokstav.
+            textBox2.Clear();
+                       
+           //För varje gissning som är fel så blir man mer hängd. guessNumber ökar med ett för varje felgissning så att rätt bild laddas.
+      
+                if (!rightAnswer.Contains(guess) && guessNumber == 1)
+                    pictureBox1.Load("fel1.gif");
+                if (!rightAnswer.Contains(guess) && guessNumber == 2)
+                    pictureBox1.Load("fel2.gif");
+                if (!rightAnswer.Contains(guess) && guessNumber == 3)
+                    pictureBox1.Load("fel3.gif");
+                if (!rightAnswer.Contains(guess) && guessNumber == 4)
+                    pictureBox1.Load("fel4.gif");
+
+                if (!rightAnswer.Contains(guess) && guessNumber == 5)  //Efter 5 gissningar så är man hängd 
+                {
+                    pictureBox1.Load("fel5.gif");
+                    btnGissa.Enabled = false;
+                    lblSlutResultat.Text = "Sorry, du är hängd! Rätt svar var " + rightAnswer2;
+                }
+
+                if (!rightAnswer.Contains(guess)) //Om gissningen inte är rätt ökas guessNumber för nästa bild.
+                    guessNumber++;
+
+                else if (rightAnswer.Contains(guess)) //Om gissningen ÄR rätt så minskas bokstäver att gissa på.
+                {
+                    
+                    while (rightAnswer.IndexOf(guess) != -1) //Om ordet innehåller mer än en bokstav av samma sort, ser denna till att alla visas.
+                    {                                        //Om du skriver in t t.ex. så letar denna upp alla t. När den inte hittar fler är index -1
+
+                        label1.Text += guess.ToString();
+
+                        char test = guess;
+
+                        int index = rightAnswer.IndexOf(guess); //Tar reda på indexet för bokstaven du gissade på
+
+                        //rightAnswer3.Replace('_', test);
+                                                  
+                        if (index != -1) //Om det inte är tomt på bokstäver
+                        {
+                         
+                             if (rightAnswer.Length == 1)
+                                btnGissa.Enabled = false;                        
+
+                              rightAnswer = rightAnswer.Remove(index, 1); //"Index" är numret för bokstaven, "1" är hur många som ska tas bort.
+                        }
+                        else
+                            btnGissa.Enabled = false;
+                    
+                    }//End of while
+                }
+                  
+        }//Här slutar btnGissa
+
+   /*     private void btnNyttOrd_Click(object sender, EventArgs e) //Knapp för att lägga in ordet att gissa
+        {
+            rightAnswer = textBox1.Text; //Det rätta svaret lägg i en variabel för att minska för varje rätt bokstav.
+            rightAnswer2 = textBox1.Text; //Det rätta svaret lägg i en variabel som inte minskar. Denna variabel visas om man gissar rätt ord.
+            textBox1.Clear();
+
+            int wordLength = rightAnswer2.Length;
+
+            for (int i = 1; i <= wordLength; i++)
+            {
+                label1.Text += "_ ";
+            }
+        
+        }  */
+
+        private void btnGissaHelaOrdet_Click(object sender, EventArgs e) //Knappen för att gissa rätt ord (Hela ordet, inte bokstav för bokstav)
+        {
+            //if (textBox3.Text.Equals(rightAnswer2))
+            if (textBox3.Text == rightAnswer2)
+                lblSlutResultat.Text = "Du gissade rätt ord: " + rightAnswer2;
+            else
+            {
+                pictureBox1.Load("fel5.jif");
+                btnGissa.Enabled = false;
+                lblSlutResultat.Text = "Sorry, du är hängd! Rätt svar var " + rightAnswer2;
+            }
+
+        }
+
+        private void ta_Click(object sender, EventArgs e)
+        {
+            rightAnswer = returnWordSwedish();
+            rightAnswer2 = rightAnswer;
+        }
+    }
+}
